9d8613ac0764f25fbca390b492be19983d70cc00
RaiderRoss <aidan1.mck@gmail.com>
2025-05-23 12:40:41 +0200

    Hashing is done!

--- backend/db/src/models/user.rs
+++ backend/db/src/models/user.rs
@@ -1,8 +1,11 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use sqlx::Row;
 use sqlx::SqlitePool;
-
 /// Represents a user in the system.
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct User {
@@ -78,6 +81,23 @@ impl User {
             .execute(pool)
             .await?;
         Ok(())
+    }
+
+    pub fn hash(password: &str) -> String {
+        let salt = SaltString::generate(&mut OsRng);
+        let argon2 = Argon2::default();
+        let password_hash = argon2
+            .hash_password(password.as_bytes(), &salt)
+            .expect("Failed to hash password")
+            .to_string();
+        password_hash
+    }
+
+    pub fn verify(password: &str, hash: &str) -> bool {
+        let hash = PasswordHash::new(hash).expect("Failed to parse password hash");
+        Argon2::default()
+            .verify_password(password.as_bytes(), &hash)
+            .is_ok()
     }
 
     /// Fetches a user by their ID.
@@ -274,6 +294,15 @@ mod tests {
         assert!(found.is_some());
         assert_eq!(found.unwrap().student_number, sn);
         crate::delete_database("test_get_by_student_number.db");
+    }
+
+    #[tokio::test]
+    async fn test_hash_and_verify() {
+        let password = "password12";
+        let hash = User::hash(password);
+        println!("Hash: {}", hash);
+        assert!(User::verify(password, &hash));
+        assert!(!User::verify("wrong_password", &hash));
     }
 
     #[tokio::test]
