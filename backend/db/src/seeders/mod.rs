12cb7457f7e0b2a34715fc8284bcb127f31598fa
Reece Jordaan <reecejordaan123@gmail.com>
2025-05-24 17:19:03 +0200

    Seeded tables now get cleared and reseeded at the start of each run

--- backend/db/src/seeders/mod.rs
+++ backend/db/src/seeders/mod.rs
@@ -5,6 +5,15 @@ pub mod user_seeder;
 use sqlx::SqlitePool;
 
 pub async fn seed(pool: &SqlitePool) {
+    log::info!("Clearing all tables...");
+    sqlx::query("DELETE FROM module_students").execute(pool).await.unwrap();
+    sqlx::query("DELETE FROM module_tutors").execute(pool).await.unwrap();
+    sqlx::query("DELETE FROM module_lecturers").execute(pool).await.unwrap();
+    sqlx::query("DELETE FROM assignments").execute(pool).await.unwrap();
+    sqlx::query("DELETE FROM modules").execute(pool).await.unwrap();
+    sqlx::query("DELETE FROM users").execute(pool).await.unwrap();
+    sqlx::query("DELETE FROM sqlite_sequence").execute(pool).await.unwrap();
+    
     module_seeder::seed(pool).await;
     user_seeder::seed(pool).await;
     assignment_seeder::seed(pool).await;
