2681b73582444a938359167639fa8066b106700c
RaiderRoss <aidan1.mck@gmail.com>
2025-08-04 14:50:05 +0200

    Easy Peasy

---
+++ backend/db/src/models/ticket_messages.rs
@@ -0,0 +1,48 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Deserialize, Serialize)]
+#[sea_orm(table_name = "ticket_messages")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i64,
+
+    pub ticket_id: i64,
+    pub user_id: i64,
+
+    pub content: String,
+
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::tickets::Entity",
+        from = "Column::TicketId",
+        to = "super::tickets::Column::Id"
+    )]
+    Ticket,
+
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::UserId",
+        to = "super::user::Column::Id"
+    )]
+    User,
+}
+
+impl Related<super::tickets::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Ticket.def()
+    }
+}
+
+impl Related<super::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
---
+++ backend/db/src/models/tickets.rs
@@ -0,0 +1,65 @@
+use sea_orm::entity::prelude::*;
+use sea_orm::{DeriveActiveEnum};
+use serde::{Deserialize, Serialize};
+use strum::{Display, EnumString};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "tickets")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i64,
+
+    pub assignment_id: i64,
+    pub user_id: i64,
+
+    pub title: String,
+    pub description: String,
+
+    pub status: TicketStatus,
+
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+}
+
+#[derive(Debug, Clone, PartialEq, EnumIter, DeriveActiveEnum, Display, EnumString, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+#[sea_orm(rs_type = "String", db_type = "Enum", enum_name = "ticket_status")]
+#[strum(serialize_all = "lowercase", ascii_case_insensitive)]
+pub enum TicketStatus {
+    #[sea_orm(string_value = "open")]
+    Open,
+
+    #[sea_orm(string_value = "closed")]
+    Closed,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::assignment::Entity",
+        from = "Column::AssignmentId",
+        to = "super::assignment::Column::Id"
+    )]
+    Assignment,
+
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::UserId",
+        to = "super::user::Column::Id"
+    )]
+    User,
+}
+
+impl Related<super::assignment::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Assignment.def()
+    }
+}
+
+impl Related<super::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
