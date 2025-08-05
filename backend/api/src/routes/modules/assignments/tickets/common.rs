8de7dfec75fffffa81f7dbb58af10144e0c49578
RaiderRoss <aidan1.mck@gmail.com>
2025-08-05 13:21:27 +0200

    Added Required to module for tickets

--- backend/api/src/routes/modules/assignments/tickets/common.rs
+++ backend/api/src/routes/modules/assignments/tickets/common.rs
@@ -6,20 +6,8 @@ use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, JoinType, QueryFilte
 use serde::{Deserialize, Serialize};
 
 pub async fn is_valid(module_id: i64, user_id: i64, ticket_id: i64, db: &DatabaseConnection) -> bool {
-    let is_student = user_module_role::Entity::find()
-        .filter(user_module_role::Column::UserId.eq(user_id))
-        .filter(user_module_role::Column::ModuleId.eq(module_id))
-        .filter(user_module_role::Column::Role.eq(Role::Student))
-        .join(JoinType::InnerJoin, user_module_role::Relation::User.def())
-        .filter(user::Column::Admin.eq(false))
-        .one(db)
-        .await
-        .map(|opt| opt.is_some())
-        .unwrap_or(false);
-
     let is_author = TicketModel::is_author(ticket_id, user_id, db).await;
-
-    !is_student && is_author
+    is_author
 }
  
  #[derive(Debug, Serialize, Deserialize)]
