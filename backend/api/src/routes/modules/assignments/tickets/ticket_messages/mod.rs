0df90db06a1996f8ccbb7dc9990a58bdd4f11061
RaiderRoss <aidan1.mck@gmail.com>
2025-08-14 14:40:13 +0200

    Done getting tickets

--- backend/api/src/routes/modules/assignments/tickets/ticket_messages/mod.rs
+++ backend/api/src/routes/modules/assignments/tickets/ticket_messages/mod.rs
@@ -1,15 +1,18 @@
-use axum::{Router, routing::{post, put, delete}};
+use axum::{Router, routing::{post, put, delete, get}};
 use util::state::AppState;
 pub mod post;
 pub mod put;
 pub mod delete;
 pub mod common;
+pub mod get;
 use post::create_message;
 use put::edit_ticket_message;
 use delete::delete_ticket_message;
+use get::get_ticket_messages;
 pub fn ticket_message_routes(_app_state: AppState) -> Router<AppState> {
 	Router::new()
 	.route("/", post(create_message))
+	.route("/", get(get_ticket_messages))
 	.route("/{message_id}", put(edit_ticket_message))
 	.route("/{message_id}", delete(delete_ticket_message))
 }
