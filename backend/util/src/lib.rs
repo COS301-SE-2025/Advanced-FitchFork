e00ddd3b6e026765814f4f5471099c19b7999f62
RKruse42 <1richardkruse@gmail.com>
2025-09-10 11:22:42 +0200

    Added scan_code_content

--- backend/util/src/lib.rs
+++ backend/util/src/lib.rs
@@ -2,5 +2,6 @@ pub mod code_coverage_report;
 pub mod config;
 pub mod execution_config;
 pub mod mark_allocator;
+pub mod scan_code_content;
 pub mod state;
 pub mod ws;
---
+++ backend/util/src/scan_code_content/mod.rs
@@ -0,0 +1,1 @@
+pub mod scan_code_content;
